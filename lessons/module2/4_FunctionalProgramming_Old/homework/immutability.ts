099a7109084aa145bb8ad1bb005bb9a398853e70
Radamir <radamir.gorynin@raiffeisen.ru>
2023-02-23 15:41:11 +0300

    HW-8: part1, part2

--- lessons/module2/4_FunctionalProgramming_Old/homework/immutability.ts
+++ lessons/module2/4_FunctionalProgramming_Old/homework/immutability.ts
@@ -1,49 +1,39 @@
 // // Задание 1
-export type OriginalTeam = {
-  size: number;
-  name: string;
-  league: string;
-};
+// export type OriginalTeam = {
+//   size: number;
+//   name: string;
+//   league: string;
+// };
 
-export type ExpectedTeam = {
-  name: string;
-  league: string;
-  roster: number;
-};
+// export type ExpectedTeam = {
+//   name: string;
+//   league: string;
+//   roster: number;
+// };
 
-export const originalTeamToExpectedTeam = (
-  originalTeam: OriginalTeam
-): ExpectedTeam => {
-  const { league } = originalTeam;
-  return {
-    league: league,
-    roster: 25,
-    name: "New York Badgers",
-  };
-};
+// export const originalTeamToExpectedTeam = (
+//   originalTeam: OriginalTeam
+// ): ExpectedTeam => {
+//   //
+// };
 
 // // Задание 2
-type SomeArray = ReadonlyArray<number | string>;
+// type SomeArray = Array<number | string>;
 
-export const originalArrayToExpectedArray = (
-  originalArray: SomeArray
-): SomeArray => {
-  const newArray = ["two", ...originalArray.slice(2), 5];
-  return newArray;
-};
+// const originalArrayToExpectedArray = (originalArray: SomeArray): SomeArray => {
+//   //
+// };
 
 // // Задание 3
 
-export type Team = {
-  name: string;
-  captain: {
-    name: string;
-    age: number;
-  };
-};
+// export type Team = {
+//   name: string;
+//   captain: {
+//     name: string;
+//     age: number;
+//   };
+// };
 
-export const originalTeamToExpectedTeamDeep = (originalTeam: Team): Team => {
-  const newObj = JSON.parse(JSON.stringify(originalTeam));
-  newObj.captain.age = 28;
-  return newObj;
-};
+// export const originalTeamToExpectedTeam = (originalTeam: Team): Team => {
+//   //
+// };
